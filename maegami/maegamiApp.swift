e1033b7e4a62b1ffa76b58a9ac717b2fcdc096cd
test <test>
2023-06-11 17:03:15 +0900

    Initial Commit

---
+++ maegami/maegamiApp.swift
@@ -0,0 +1,20 @@
+//
+//  maegamiApp.swift
+//  maegami
+//
+//  Created by sayakoakamatsu on 2023/06/11.
+//
+
+import SwiftUI
+
+@main
+struct maegamiApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
